5697d8338250b6a6e7654d3f55af0aa4481d6b58
Harshit Omer <omerharshit1997@gmail.com>
2017-05-06 01:35:34 +0530

    Add files via upload

---
+++ PHP/reveal.php
@@ -0,0 +1,342 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<title>Project Presentation| Harshit Omer </title>
+
+		<link rel="stylesheet" href="/index.html/CSS_reveal/reveal.css">
+		<link rel="stylesheet" href="/index.html/CSS_reveal/black.css">
+		
+
+		<!-- Theme used for syntax highlighting of code -->
+		<link rel="stylesheet" href="/index.html/CSS_reveal/zenburn.css">
+
+		<!-- Printing and PDF exports -->
+		<script>
+			var link = document.createElement( 'link' );
+			link.rel = 'stylesheet';
+			link.type = 'text/css';
+			link.href = window.location.search.match( /print-pdf/gi ) ? '/index.html/CSS_reveal/pdf.css' : '/index.html/CSS_reveal/paper.css';
+			document.getElementsByTagName( 'head' )[0].appendChild( link );
+		</script>
+	</head>
+	<body>
+	<style type="text/css">
+		
+.box-image{
+  padding-right: 2px;
+  padding-left: 2px;
+
+}
+.box-image img  
+{
+  max-width: 100%;
+  min-height: 250px;
+  max-height: 250px;
+  min-width: auto;
+  }
+  
+.box-content {
+  padding: 20px;
+  font-family: "Comic Sans MS", cursive, sans-serif;
+}
+.box-title {
+  padding:10px;
+  font-size:24px;
+}
+
+	</style>
+
+		<div class="reveal">
+			<div class="slides">
+		<section>Hey! My projects </section>
+		<section>MY PASSION MY SKILLS </section>
+	    <section>
+				<div class="col s12 m12 l4">
+            <div class="row box">
+              <h5 class="box-title" >1. CODEing</h5>
+               <div class="col s12 m12 l12 box-image" >
+                <img src="/index.html/photos/code.jpg">
+               </div>
+            <div class="col s12 m12 l12 box-content"  >
+                coding is my favorite part time work. I have good command in C,C++,C#,HTML,CSS,Javascript,Python,php.<br>
+            </div>
+          </div>
+        </div>
+       </section>
+       <section>
+        <div class="col s12 m12 l4">
+            <div class="row box">
+            <h5 class="box-title" >2. Sudoku</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/sudoku2.jpg">
+              </div>
+              <div class="col s12 m12 l12 box-content" style="color: green;" >
+                 This is also game of Mind and Time.You can be judge your Accuracy skill by solving SUDUKO.<br>
+                </div>
+              </div>
+            </div>
+        </section>
+<section>
+	<div class="col s12 m12 l4">
+            <div class="row box">
+              <h5 class="box-title">3. Solitaire Games</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/solitaire.jpg">
+              </div>
+              <div class="col s12 m12 l12 box-content">
+                 solitaire game series have various games. All are superb and interasting. <br>
+             </div>
+            </div>
+           </div>
+</section>
+<section>
+	 <div class="col s12 m12 l4">
+            <div class="row box">
+            <h5 class="box-title" >4.Cube</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/cube2.jpg">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+                  Cube solving is a MIND and CONCEPT Game.Time is the important part of this Game.I can complete whole cube in 2 minutes.<br>
+              </div>
+            </div>
+          </div>
+</section>
+
+<section>
+	
+	<div class="col s12 m12 l4">
+            <div class="row box">
+              <h5 class="box-title" >5.Asphalt </h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/asphalt.jpg">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+                 I like racing games NFS and Asphalt are both my favorite. <br>
+              </div>
+            </div>
+          </div>
+</section>
+<section>
+	
+	 <div class="col s12 m12 l4">
+            <div class="row box">
+              <h5 class="box-title" >6.Philosophy</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/philosophy.jpg">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+                I am  not really a pholosopher.But I want to be like Aristotle so I try to think like him.
+              </div>
+            </div>
+          </div>
+        
+</section>
+<section>
+	
+	 <div class="row">
+          <div class="col s12 m12 l4">
+            <div class="row box">
+            <h5 class="box-title" >7. Writter</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/writter.jpg">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+                I wrote many stories in my school days. Writing poems is my favorite part time work from school days.
+              </div>
+            </div>
+          </div>
+        
+</section>
+		<section>
+			
+			<p >Now, My Computer Science Projects!!</p>
+		</section>
+<section>
+	
+	 <div class="col s12 m12 l4">
+            <div class="row box">
+              <h5 class="box-title" >8. Octo Game</h5>
+               <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/octogame.png">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+                Simple strategy game more like clash of clans but in 1D, built using Php
+                and Mysql and cron jobs for updatation. <br>
+              </div>
+            </div>
+          </div>
+</section>
+		<section>
+			
+			<div class="col s12 m12 l4">
+            <div class="row box">
+              <h5 class="box-title" >9. Abhivyakti 2k18</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/abhivyakti.jpg">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+                devloping a website for my colleges cultural fest named Abhivyakti. 
+                <br>
+              </div>
+            </div>
+          </div>
+		</section>
+<section>
+	
+	 <div class="col s12 m12 l4">
+            <div class="row box">
+              <h5 class="box-title" >11. Laptop Selfi</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/selfie.png">
+              </div>
+              <div class="col s12 m12 l12 box-content">
+                Capture, edit and share selfie's now with your webcam.
+                using getMedia API of chrome i will built this.[WORK ON PROCESS]<br>
+              </div>
+            </div>
+          </div>
+</section>
+<section>
+	
+	<div class="row">
+          <div class="col s12 m12 l4">
+            <div class="row box">
+             <h5 class="box-title">13. Gantavya 2k18</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/gantavya.png">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+                Creating a website for my colleges technical fest named Gantavya 2k18. 
+                I am devloping this website using Php and JQuery. <br>
+               </div>
+            </div>
+          </div>
+</section>
+<section>
+	
+	<div class="col s12 m12 l4">
+            <div class="row box">
+             <h5 class="box-title" >14. Notification Panel</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/notification_panel.png">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+                Created a software using javascript which gives notification on your PC on a single click.
+                I hope you all will like it. <br>
+               </div>
+               <div class="button-box">
+                <a class="wonder-links" href="https://github.com/omerharshit/Notification_Panel" target="_blank" style="color: black; background: transparent; margin-left:  50px;">Code</a>
+                <a class="wonder-links wonder-link-right" href="/index.html/HTML/Notification_Panel/index.php" target="_blank" style="color: black; background: transparent; margin-left:  50px;">Demo</a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+           <div class="col s12 m12 l4">
+            <div class="row box">
+             <h5 class="box-title" >15. Online Calculator </h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/calci.png">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+                Created a online calculator which used for accounting.that calculator is created in javascript language.
+                I hope you all will like it. <br>
+               </div>
+               <div class="button-box">
+                <a class="wonder-links" href="https://github.com/omerharshit/Calculator" target="_blank" style="color: black; background: transparent; margin-left:  50px;">Code</a>
+                <a class="wonder-links wonder-link-right" href="/index.html/HTML/Calculator/index.html" target="_blank" style="color: black; background: transparent; margin-left:  50px;">Demo</a>
+              </div>
+            </div>
+          </div>
+        </section>
+      <section>
+         <div class="row">
+          <div class="col s12 m12 l4">
+            <div class="row box">
+             <h5 class="box-title">16. Down-for-everyone-or-for-me</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/DFEOFM.png">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+                Creating a webpage using PHP API which tells that given URL available in internet or not. <br>
+               </div>
+                 <div class="button-box">
+                <a class="wonder-links" href="https://github.com/omerharshit/Down-for-everyone-or-for-me" target="_blank" style="color: black; background: transparent; margin-left:  50px;">Code</a>
+                <a class="wonder-links wonder-link-right" href="/index.html/HTML/Down-for-everyone-or-for-me/index.php" target="_blank" style="color: black; background: transparent; margin-left:  50px;">Demo</a>
+              </div>
+            </div>
+          </div>
+
+      </section>
+      <section>
+        <div class="col s12 m12 l4">
+            <div class="row box">
+             <h5 class="box-title">17.My Website V.1</h5>
+              <div class="col s12 m12 l12 box-image">
+                <img src="/index.html/photos/website_v.1.png">
+              </div>
+              <div class="col s12 m12 l12 box-content" >
+               My first website created using HTML,CSS,PHP,JAVASCRIPT and PHYTHON languages. <br>
+               </div>
+               <div class="button-box" >
+               <div style="color: white; background: transparent; margin-right:  50px;" ><a  class="wonder-links"  href="https://github.com/omerharshit/My-website-v.1" target="_blank" style="color: black; background: transparent; margin-right:  50px;">Code</a>
+               <a class="wonder-links wonder-link-right" href="/index.html/HTML/My-website-v.1/My_Website_v.1.php" target="_blank" style="color: black; background: transparent; margin-left:  50px;">Demo</a></div>
+               </div>
+               </div>
+               </div>
+   <style type="text/css">
+  .wonder-links 
+{
+  color: white;
+  background-color: black;
+  padding: 6px 15px ;
+  margin-top: 10px;
+  background: transparent;
+}
+.wonder-link-right {
+  margin-right:30px;
+  background: transparent;
+  color: white;
+
+}
+.button-box {
+  position: relative;
+  width: calc(100% - 10px);
+ margin-top: 20px;
+ margin-bottom: 10px;
+  padding-left: 10px;
+  margin-right: 40px;
+  background: transparent;
+  color: white;
+}
+               </style>
+      </section>
+        	<section>
+        	<p style="color: red; font-size: 50px;">Thanks, For liking it!</p>
+        </section>
+			</div>
+		</div>
+
+		<script src="/index.html/js_reveal/head.min.js"></script>
+		<script src="/index.html/js_reveal/reveal.js"></script>
+
+		<script>
+			// More info https://github.com/hakimel/reveal.js#configuration
+			Reveal.initialize({
+				history: true,
+
+				// More info https://github.com/hakimel/reveal.js#dependencies
+				dependencies: [
+					{ src: 'plugin/markdown/marked.js' },
+					{ src: 'plugin/markdown/markdown.js' },
+					{ src: 'plugin/notes/notes.js', async: true },
+					{ src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+				]
+			});
+		</script>
+	</body>
+</html>
